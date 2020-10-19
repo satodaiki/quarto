856eb9923541aa6a8c54216ac0eef1bb885812bf
sato daiki <satodaiki.business@gmail.com>
2020-10-20 07:44:05 +0900

    Pieceインターフェースの実装

--- src/components/pages/Main.vue
+++ src/components/pages/Main.vue
@@ -1,14 +1,40 @@
 <template>
     <v-container>
-        hello world.
+        <v-row>
+            <v-col>
+                <!-- board -->
+                <v-container>
+                    <v-row>
+                        <v-col>
+                        </v-col>
+                    </v-row>
+                </v-container>
+                <!-- piece stack -->
+                <v-container>
+                    <v-row>
+                        <v-col>
+                        </v-col>
+                    </v-row>
+                </v-container>
+            </v-col>
+        </v-row>
     </v-container>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 
+interface Piece {
+    hole: 'yes' | 'no'
+    height: 'low' | 'high'
+    color: 'dark' | 'light'
+    form: 'rectangle' | 'circle'
+}
+
 @Component({
   name: 'Main',
 })
-export default class extends Vue {}
+export default class extends Vue {
+    private pieces: Piece[] = []
+}
 </script>
