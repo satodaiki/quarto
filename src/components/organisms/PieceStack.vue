905077069eed5a8037fe1fe2d9d28d01b0286806
sato daiki <satodaiki.business@gmail.com>
2020-10-23 07:56:20 +0900

    Main画面の実装を追加

---
+++ src/components/organisms/PieceStack.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import { Piece } from '@/components/pages/Main.vue';
+
+@Component({
+  name: 'PieceStack',
+})
+export default class extends Vue {
+    @Prop()
+    private pieces: Piece[] = []
+
+    mounted() {
+        console.log(this.pieces);
+    }
+}
+</script>
--- src/components/pages/Main.vue
+++ src/components/pages/Main.vue
@@ -1,40 +1,80 @@
 <template>
-    <v-container>
-        <v-row>
-            <v-col>
-                <!-- board -->
-                <v-container>
-                    <v-row>
-                        <v-col>
-                        </v-col>
-                    </v-row>
-                </v-container>
-                <!-- piece stack -->
-                <v-container>
-                    <v-row>
-                        <v-col>
-                        </v-col>
-                    </v-row>
-                </v-container>
-            </v-col>
-        </v-row>
-    </v-container>
+  <v-container>
+    <v-row>
+      <v-col>
+        <!-- board -->
+        <v-container>
+          <v-row>
+            <v-col> </v-col>
+          </v-row>
+        </v-container>
+        <!-- piece stack -->
+        <piece-stack :pieces="pieceStack" />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+<script lang='ts'>
+import { Component, Vue, Ref } from 'vue-property-decorator';
+import PieceStack from '@/components/organisms/PieceStack.vue';
 
-interface Piece {
-    hole: 'yes' | 'no'
-    height: 'low' | 'high'
-    color: 'dark' | 'light'
-    form: 'rectangle' | 'circle'
+export const PieceHole = {
+  YES: 'yes',
+  NO: 'no',
+} as const;
+type PieceHole = typeof PieceHole[keyof typeof PieceHole];
+
+export const PieceHeight = {
+  LOW: 'low',
+  HIGH: 'high',
+} as const;
+type PieceHeight = typeof PieceHeight[keyof typeof PieceHeight];
+
+export const PieceColor = {
+  DARK: 'dark',
+  LIGHT: 'light',
+} as const;
+type PieceColor = typeof PieceColor[keyof typeof PieceColor];
+
+export const PieceForm = {
+  RECTANGLE: 'rectangle',
+  CIRCLE: 'circle',
+} as const;
+type PieceForm = typeof PieceForm[keyof typeof PieceForm];
+
+export interface Piece {
+  hole: PieceHole;
+  height: PieceHeight;
+  color: PieceColor;
+  form: PieceForm;
 }
 
 @Component({
   name: 'Main',
+  components: {
+    PieceStack,
+  },
 })
 export default class extends Vue {
-    private pieces: Piece[] = []
+  private pieceStackPieces: Piece[] = [];
+  private boardPieces: Piece[] = [];
+
+  async mounted() {
+    for (const form of Object.values(PieceForm)) {
+      for (const color of Object.values(PieceColor)) {
+        for (const height of Object.values(PieceHeight)) {
+          for (const hole of Object.values(PieceHole)) {
+            this.pieceStackPieces.push({
+              hole,
+              height,
+              color,
+              form,
+            });
+          }
+        }
+      }
+    }
+  }
 }
 </script>
