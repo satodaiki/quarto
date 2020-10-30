0aee32cb9ffaba10c27e2336164cf3654939ff30
satodai <banana5388@gmail.com>
2020-10-30 23:33:31 +0900

    クアルトボタンの実装を追加

--- src/components/pages/Main.vue
+++ src/components/pages/Main.vue
@@ -6,6 +6,7 @@
     <v-row class="my-4" justify="center">
       <v-btn
         x-large
+        @click="result()"
       >
         Quarto!
       </v-btn>
@@ -32,7 +33,7 @@
     <ResultNotification
       :show="showResultNotification"
       :playerName="currentPlayerName()"
-      :result="result"
+      :result="resultState"
     />
   </v-container>
 </template>
@@ -69,7 +70,7 @@ export default class extends Vue {
 
   private showResultNotification = false;
 
-  private result?: boolean;
+  private resultState?: boolean;
 
   private currentPlayerName() {
     return this.gameField.currentPlayer.playerId;
@@ -78,14 +79,14 @@ export default class extends Vue {
   private setBoardPiece(payload: { width: number; height: number }) {
     if (this.selectPieceId !== null) {
       this.toggleDisabled();
-      this.result = this.gameField.setPiece(payload.height, payload.width);
-      if (this.result) this.showResultNotification = true;
+      this.gameField.setPiece(payload.height, payload.width);
       this.boardKey += 1;
-      // if (result) {
-      //   // eslint-disable-next-line no-alert
-      //   alert(`${this.gameField.currentPlayer.playerId}の勝ちだよ`);
-      // }
     }
+  }
+
+  private result() {
+    this.showResultNotification = true;
+    this.resultState = this.gameField.getJudgeResult();
   }
 
   private toggleDisabled() {
